c1b1a804836210a319c2d379c000a88343a5bde3
Hardik Patel <hardikgw@gmail.com>
2019-01-30 14:37:21 -0500

    samples

--- claims-book.py
+++ claims-book.py
@@ -33,8 +33,9 @@ def extract_quads(pages):
     op_file = open(quads_file, "a")
     has_more = True
     skip = 0
-    limit = 10
+    limit = 100
     while has_more == True and pages > 0:
+        print("pages=%s skip=%s limit=%s" % (pages, skip, limit))
         stmt: str = f"""match (v1:vendors)-[r1:worked_with]-(v2:vendors)-[r2:worked_with]-(v3:vendors)-[r3:worked_with]-(v4:vendors)
                 where r1.claimnumber = r2.claimnumber = r3.claimnumber and id(v1) < id(v2) < id(v3) < id(v4)
                 return v1.id, v2.id, v3.id, v4.id, r1.claimnumber, r2.claimnumber, r3.claimnumber skip {skip} limit {limit}"""
@@ -44,11 +45,11 @@ def extract_quads(pages):
             has_more = False
         else:
             skip += limit
-            limit += limit
         for record in result:
             result_line = ""
             for key in record.keys():
                 result_line += record[key] + ","
             op_file.write(result_line + '\n')
+
 
 extract_quads(100000)
