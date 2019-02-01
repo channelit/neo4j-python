4ccf9cc29a6b6b9967042409f9068e9001f61f2a
Hardik Patel <hardikgw@gmail.com>
2019-02-01 12:43:12 -0500

    venv reqs

--- docker/commands.sh
+++ docker/commands.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 7474:7474 -p 7687:7687 -v $HOME/neo4j/data:/data -v $HOME/neo4j/logs:/logs -e NEO4J_dbms_memory_heap_max__size=3096M  --name=neo neo4j
+docker run -p 7474:7474 -p 7687:7687 -v $HOME/neo4j/data:/data -v $HOME/neo4j/logs:/logs -e NEO4J_dbms_memory_heap_max__size=3096M --name=neo neo4j
